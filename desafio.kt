fbb36279ec8f0cd161e7365dffa10cd150148e00
f4k3z0rd <vinicius.meireles@outlook.com>
2023-05-05 17:00:53 -0300

    final commit

--- desafio.kt
+++ desafio.kt
@@ -49,7 +49,6 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     private val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-//        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
         inscritos.add(usuario).also { usuario.conteudosInscritos.add(this) }
     }
 
@@ -71,8 +70,6 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
-//    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-//    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 
     val usuario1 = Usuario("Jolteon")
     val usuario2 = Usuario("Flareon")
